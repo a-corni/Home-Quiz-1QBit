3cba9ce4a7e7f063d0a58051f2e2d30a78f22cb5
acorni <antoine.cornillot@student-cs.fr>
2020-04-09 11:43:31 +0200

    Add error handling

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import Graph
 import Representation
 import Analysis
+import random as rd
 
-n = 10
+n = 8
 
 graph = generate_graph(n)
 
-represent_graph(graph, n)
+all_x, all_y = represent_graph(graph, n)
+
+x = rd.randint(0,n-1)
+y = rd.randint(0,n-1)
+
+if x == y:
+   
+    print("x and y have same value : No paths")
+
+else :
+
+    paths = all_hamiltonian_paths(graph, n, x, y)
+    
+    if len(paths) == 0:
+     
+        print("no hamiltonian_paths between "+ str(x) + " and " + str(y))
+    
+    else :
+    
+        print(x, y)
+        represent_paths(all_x, all_y, paths)
+        print(to_graph(paths,n))
